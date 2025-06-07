3cbdd4da0821f6d09a0007bbec3971a56a95338f
sharmilidash <dashsharmili7@gmail.com>
2025-06-07 10:08:21 +0530

    Update Google_Homepage.spec.js
    updated from github

--- tests/UI_Test/Google_Homepage.spec.js
+++ tests/UI_Test/Google_Homepage.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
 
     await page.goto("https://www.google.co.in/")
-    await page.waitForLoadState("networkidle")
+    await page.waitForLoadState("load")
     await page.waitForTimeout(1000)
     await page.locator("//textarea[@id='APjFqb']").fill("Plawright")
     await page.locator("//textarea[@id='APjFqb']").click({key: "Enter"})
